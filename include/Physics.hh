58cad6d615535dc9dadeee42fe665d75f1541230
victorhugogarccia <144040116+victorhugogarccia@users.noreply.github.com>
2023-09-27 19:05:20 -0600

    Update Physics.hh

--- include/Physics.hh
+++ include/Physics.hh
@@ -1,13 +1,14 @@
- #ifndef PHYSICS_HH
- #define PHYSICS_HH
- 
- #include "G4VModularPhysicsList.hh"
- #include "G4EmStandardPhysics.hh"
- 
- class PhysicsList : public G4VModularPhysicsList{
- public:
-    PhysicsList();
-    ~PhysicsList();
- };
- 
- #endif
+#ifndef PHYSICS_HH
+#define PHYSICS_HH
+
+#include "G4VModularPhysicsList.hh"
+#include "G4EmStandardPhysics.hh"
+
+class PhysicsList : public G4VModularPhysicsList
+{
+public:
+	PhysicsList();
+	~PhysicsList();
+};
+
+#endif
