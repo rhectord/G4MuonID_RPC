1c89979465ad8d347881d22ab7d7f2840a00700e
victorhugogarccia <144040116+victorhugogarccia@users.noreply.github.com>
2023-09-27 19:06:25 -0600

    Update PrimaryGeneratorAction.hh

--- include/PrimaryGeneratorAction.hh
+++ include/PrimaryGeneratorAction.hh
@@ -2,32 +2,22 @@
 #define PRIMARY_GENERATOR_ACTION_HH
 
 #include "G4VUserPrimaryGeneratorAction.hh"
+
 #include "G4ParticleGun.hh"
-#include "G4MuonMinus.hh" 
 #include "G4SystemOfUnits.hh"
 #include "G4ParticleTable.hh"
-//#include "G4VisAttributes.hh"
-//#include "G4Tubs.hh"
-#include "Randomize.hh"
+#include "G4VisAttributes.hh"
 
-#include <cmath>
-#include <math.h>
-
-class DetectorConstruction; //¿?
-
-/// Action initialization class
-
-class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction {
-
-  public:
-    PrimaryGeneratorAction();
-    virtual ~PrimaryGeneratorAction();
-
-    virtual void GeneratePrimaries(G4Event*);
-    
-  private:
-    G4ParticleGun *fParticleGun;
-    
+class PrimaryGeneratorAction: public G4VUserPrimaryGeneratorAction
+{
+public:
+	PrimaryGeneratorAction();
+	~PrimaryGeneratorAction();
+	
+	virtual void GeneratePrimaries(G4Event*);
+	
+private:
+	G4ParticleGun *fParticleGun;
 };
 
 #endif
