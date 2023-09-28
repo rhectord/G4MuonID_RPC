a950c56e5b4480b8c6a2769bcbebed925e35ad1c
victorhugogarccia <144040116+victorhugogarccia@users.noreply.github.com>
2023-09-27 19:04:57 -0600

    Update DetectorConstruction.hh

--- include/DetectorConstruction.hh
+++ include/DetectorConstruction.hh
@@ -4,22 +4,30 @@
 #include <iostream>
 #include <string>
 
-#include "G4VUserDetectorConstruction.hh"
 #include "globals.hh"
+#include "G4VUserDetectorConstruction.hh"
+
+#include "G4VUserDetectorConstruction.hh"
+#include "G4VPhysicalVolume.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+#include "G4NistManager.hh"
 #include "G4SystemOfUnits.hh"
+#include "DetectorConstruction.hh"
 
 #include "SensitiveDetector.hh"
 
-class DetectorConstruction : public G4VUserDetectorConstruction {
-
-   public:
-      DetectorConstruction();
-      ~DetectorConstruction();
+class DetectorConstruction : public G4VUserDetectorConstruction
+{
+public:
+	DetectorConstruction();
+	~DetectorConstruction();
 	
-   virtual G4VPhysicalVolume *Construct();
-
-   private: 
+	virtual G4VPhysicalVolume *Construct();
+private: 
 	G4LogicalVolume *logicStripR1;
+	G4LogicalVolume *logicStripR2;
+	G4LogicalVolume *logiccil;
 	virtual void ConstructSDandField();
 };
 
